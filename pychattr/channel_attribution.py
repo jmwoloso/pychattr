80ab45aa251511aefd830d017bec86125ea74165
jason wolosonovich <jason@avaland.io>
2018-07-19 17:21:17 -0700

    ENH: first iteration of PyChAttr model class

--- pychattr/channel_attribution.py
+++ pychattr/channel_attribution.py
@@ -1,15 +1,23 @@
 import ctypes
-
+import pymc3
+import networkx as nx
 import pandas as pd
+from hmmlearn.hmm import GaussianHMM
+from pomegranate import State, HiddenMarkovModel
+from pomegranate.distributions import DiscreteDistribution
+from rpy2.robjects import pandas2ri, r, NULL
+from rpy2.robjects.vectors import DataFrame
+from rpy2.robjects.packages import importr, data
+from rpy2.robjects.lib import ggplot2
 
 
 class PyChAttr(object):
-    def __init__(self, df=None, model_type="markov",
+    def __init__(self, df=None, model_type="both",
                  model_paradigm=None, path_feature=None,
                  conversion_feature=None,
                  conversion_value_feature=None, null_path_feature=None,
                  separator=">", order=1, n_simulations=None,
-                 max_step=None, return_transition_probs=False,
+                 max_step=None, return_transition_probs=True,
                  random_state=None):
         """
 
@@ -17,20 +25,24 @@ class PyChAttr(object):
         ----------
         df : pandas.DataFrame containing the preprocessed path data.
 
-        model_type : str; one of {'markov', 'heuristic'};
-          default='markov'; required.
+        model_type : str; one of {'markov', 'heuristic', 'both'};
+          default='both'; required.
 
           the type of channel attribution model to construct.
 
         model_paradigm :  str or None; one of {'first_touch',
-          'last_touch', 'linear', None}; default=None; required.
+          'last_touch', 'linear', 'all_heuristics', 'markov', 'all'};
+          default='all'; required.
 
           the type of modeling paradigm to use.
 
-          the 'first_touch', 'last_touch' and 'linear' paradigms can
-          only be specified when `model_type='heuristic'` and
-          subsequently, when using `model_type='markov'` this value
-          must be set to None.
+          the 'first_touch', 'last_touch', 'linear' and 'all_heuristics'
+          paradigms will are used with `model_type='heuristic'`.
+
+          the `markov` paradigm will be used when `model_type='markov'`.
+
+          the `all` paradigm will be used when `model_type='both'` and
+          will be the combination of the above.
 
         path_feature : str; default=None; required.
           the name of the column in df containing the paths.
@@ -84,54 +96,165 @@ class PyChAttr(object):
         self.return_transition_probs = return_transition_probs
         self.random_state = random_state
 
-    def fit(self, X, y):
+    def fit(self):
         """fit the dataframe and produce the model."""
 
         # light input param validation
         self._validate_params()
 
-        # markovian model
-        if self.model_type == "markov":
-            pass
+        # turn on conversion
+        pandas2ri.activate()
 
-        # one of {first_touch, last_touch, linear}
-        if self.model_type == "heuristic":
-            pass
+        # load the appropriate R packages
+        baseR = importr("base")
+        ChannelAttributionR = importr("ChannelAttribution")
+        reshapeR = importr("reshape")
+        ggplot2R = importr("ggplot2")
 
-    def predict(self, X):
-        pass
+        r_packages = [
+            baseR,
+            ChannelAttributionR,
+            reshapeR,
+            ggplot2R
+        ]
+
+        # convert the pandas.DataFrame to an R Vector List
+        # adds two new attrs self.r_list_ and self.r_model_params_
+        self._python_params_to_r_objects(r_packages=r_packages)
+
+        # if self.model_type == "both":
+        #     self.models = list()
+        #
+        #     self._heuristic_model(r_packages=r_packages)
+        #
+        #     self.model_ = self._markov_model(r_packages=r_packages)
+        #
+        # # one of {first_touch, last_touch, linear} heuristic model
+        # if self.model_type == "heuristic":
+        #     self.model_ = self._heuristic_model(r_packages=r_packages)
+        #
+        # # markov model
+        # if self.model_type == "markov":
+        #     self.model_ = self._markov_model(r_packages=r_packages)
+
+        return self
+
+    # def transform(self):
+    #     pass
+    #
+    # def fit_transform(self):
+    #     pass
 
     def _validate_params(self):
         """Lightweight validation effort."""
         # model_type
-        if self.model_type not in ["markov", "heuristic"]:
+        if self.model_type not in ["markov", "heuristic", "both"]:
             raise ValueError("`model_type` must be one of {'markov', "
-                             "'heuristic'}")
+                             "'heuristic', 'both'}")
 
         # validate by model_type
+        if self.model_type == "both":
+            if self.model_paradigm != "all":
+                raise ValueError("`model_paradigm` must be 'all' "
+                                 "when `model_type='both'`")
         if self.model_type == "markov":
             # model_paradigm must align with model_type
-            if self.model_paradigm != None:
-                raise ValueError("`model_paradigm` must be None when "
-                                 "using 'markov' `model_type`")
+            if self.model_paradigm != "markov":
+                raise ValueError("`model_paradigm` must be 'markov' "
+                                 "when `model_type='markov'`")
 
         if self.model_type == "heuristic":
             # model_paradigm
             if self.model_paradigm not in \
-                    ["first_touch", "last_touch", "linear"]:
+                    ["first_touch", "last_touch", "linear",
+                     "all_heuristics"]:
                 raise ValueError("`model_paradigm` must be one of "
-                                 "{None, 'first_touch', 'last_touch', "
-                                 "'linear'")
+                                 "{'first_touch', 'last_touch', "
+                                 "'linear', 'all_heuristics'} when"
+                                 "`model_type='heuristic'`")
 
         # check the separator is the appropriate length
         if len(self.separator) != 1:
             raise ValueError("`separator` must have length 1")
 
-    def _heuristic_model(self):
+
+
+    def _python_params_to_r_objects(self, r_packages=None):
+        """Converts python objects to the appropriate R objects."""
+        # get the base R namespace
+        baseR = r_packages[0]
+
+        # convert the pandas.DataFrame to an R list
+        self.r_list_ =  baseR.split(DataFrame(self.df),
+                           baseR.seq(baseR.nrow(DataFrame(self.df))))
+
+        # convert the model params to strings in R
+        self.r_model_params_ = {
+            "path_feature": baseR.toString(self.path_feature),
+            "conversion_feature":
+                baseR.toString(self.conversion_feature),
+            "conversion_value_feature":
+                baseR.toString(self.conversion_value_feature),
+            "null_path_feature": baseR.toString(self.null_path_feature),
+            "separator": baseR.toString(self.separator),
+            "order": baseR.toString(self.order),
+            "n_simulations": baseR.toString(self.n_simulations),
+            "max_step": baseR.toString(self.max_step),
+            "return_transition_probs":
+                baseR.as_logical(self.return_transition_probs),
+            "random_state": baseR.as_integer(self.random_state) if
+            self.random_state != None else NULL
+        }
+
+        return self
+
+
+
+    @classmethod
+    def _heuristic_model(cls, r_list=None, path_feature=None,
+                         conversion_feature=None,
+                         conversion_value_feature=None,
+                         separator=None, r_packages=None):
+        """Creates the heuristic models."""
+
+        """
+        self, df=None, path_feature=None,
+                         conversion_feature=None,
+                         conversion_value_feature=None,
+                         separator=None
+        """
+        baseR = r_packages[0]
+        ChannelAttribution = r_packages[1]
+        reshapeR = r_packages[2]
+        ggplot2R = r_packages[3]
+
+        model = ChannelAttribution.heuristic_model(
+            r_list,
+
+
+        )
+
+
+    @classmethod
+    def _markov_model(cls, r_list=None, path_feature=None,
+                      conversion_feature=None,
+                      conversion_value_feature=None,
+                      null_path_feature=None, model_order=1,
+                      n_simulations=None, max_step=None,
+                      return_transition_matrix=None, separator=None,
+                      random_state=None, r_packages=None):
+        """Creates the hidden markov models."""
+
+        """
+        df=None, path_feature=None,
+                      conversion_feature=None,
+                      conversion_value_feature=None,
+                      null_path_feature=None, model_order=1,
+                      n_simulations=None, max_step=None,
+                      return_transition_matrix=None, separator=None
+        """
         pass
 
-    def _markov_model(self):
-        pass
 
 
 
