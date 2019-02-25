683696136ec23674be332d4d9d8ce5680a1b2238
jason wolosonovich <jason@avaland.io>
2019-02-24 22:30:14 -0700

    DOC: updated gitignore and added metadata to init

---
+++ v0.2.0/__init__.py
@@ -0,0 +1,17 @@
+"""
+Marketing Attribution module for Python
+=======================================
+
+pychattr is a Python library providing a variety of canonical
+marketing attribution models including Heurisitic (e.g. first-touch,
+last-touch, etc.), Markov Chain, Shapley Value and Halo Effect via a
+streamlined sklearn-like API.
+"""
+#TODO: versioning
+__version__ = ""
+__author__ = "Jason Wolosonovich, Brett Nebeker, Abhi Sivasailam & " \
+             "Ankur Chawla"
+__license__ = "BSD 3-Clause"
+__maintainer__ = "Jason Wolosonovich, Brett Nebeker, Abhi Sivasailam " \
+                 "& Ankur Chawla"
+__email__ = "pychattr@avaland.io"
