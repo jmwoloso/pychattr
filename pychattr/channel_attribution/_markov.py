b15d94f2c5d70967c8e1f544cf960ee31fb9e43e
jason wolosonovich <jason@avaland.io>
2019-03-12 02:24:24 -0700

    ENH: implemented transition matrix logic, fml

--- pychattr/channel_attribution/_markov.py
+++ pychattr/channel_attribution/_markov.py
@@ -1,65 +1,100 @@
 """
 Contains the model-fitting logic used for the Markov model.
 """
-import tensorflow as tf
-# use eager execution for easier debugging
-tfe = tf.contrib.eager
-use_tf_eager = True
+# Author: Jason Wolosonovich <jason@avaland.io>
+# License: BSD 3-clause
+import itertools
 
-import tensorflow_probability as tfp
-tfd = tfp.distributions
-tfb = tfp.bijectors
+import numpy as np
 
 
-if use_tf_eager:
-    try:
-        tf.enable_eager_execution()
-        print("Running TF Eager Execution.")
-    except:
-        print("Running Without TF Eager Execution.")
+def _get_transition_matrix(transitions, unique_channels):
+    """
+    Creates the transition matrix from the supplied transitions
+    and set of unique channels.
 
-def _evaluate(tensors):
-    """Evaluates Tensor or EagorTensor to Numpy ndarrays"""
-    if tf.executing_eagerly():
-        return tf.contrib.framework.nest.pack_sequence_as(
-            tensors,
-            [t.numpy() if tf.contrib.framework.is_tensor(t) else t
-             for t in tf.contrib.framework.nest.flatten(tensors)]
-        )
-    return sess.run(tensors)
+    Adapted from: https://gist.github.com/tg12/d7efa579ceee4afbeaec97eb442a6b72
+    """
+    # n x n matrix
+    n = len(unique_channels)
 
+    M = [[0] * n for _ in range(n)]
 
-def _session_options(enable_gpu_ram_resizing=True, enable_xla=False):
-    """Make use of GPUs if available."""
-    config = tf.ConfigProto()
-    config.log_device_placement=True
-    if enable_gpu_ram_resizing:
-        config.gpu_options.allow_growth=True
-    if enable_xla:
-        config.graph_options.optimizer_options.global_jit_level = (
-            tf.OptimizerOptions.ON_1
-        )
-    return config
+    for transition in transitions:
+        for (i,j) in zip(transition, transition[1:]):
+            M[i][j] += 1
+
+    for row in M:
+        s = sum(row)
+        if s > 0:
+            row[:] = [f / s for f in row]
+
+    return np.asarray(M)
 
 
-def _reset_sess(config=None):
-    """Convenience function to create TF graph & session or reset
-    them."""
-    if config is None:
-        config = session_options(enable_gpu_ram_resizing=True,
-                                 enable_xla=False)
-    global sess
-    tf.reset_default_graph()
-    try:
-        sess.close()
-    except:
-        pass
-    sess = tf.InteractiveSession(config=config)
-_reset_sess()
+def _get_markov_components(df, path_f):
+    """Calculate the transition probability matrix for the given
+    paths."""
+    # get the paths
+    paths = df.loc[:, path_f].values
+
+    # container for the channels
+    channels = []
+
+    # iterate through the paths and append the channels
+    for path in paths:
+        for channel in path:
+            channels.append(channel)
+
+    # get a list of unique channels
+    unique_channels = np.unique(channels)
+
+    # map channel -> int for calculating the transition matrix
+    channel_mapper = {channel: i for i, channel in enumerate(
+        unique_channels)}
+
+    # unmap int -> channel
+    channel_unmapper = {v: k for k, v in channel_mapper.items()}
+
+    # container for the mapped paths
+    mapped_paths = []
+
+    # iterate through the paths and map them to ints
+    for i, path in enumerate(paths):
+        mapped_channels = []
+        for channel in path:
+            mapped_channels.append(channel_mapper[channel])
+        mapped_paths.append(mapped_channels)
+
+    # calculate the transition matrix
+    trans_mat = _get_transition_matrix(mapped_paths, unique_channels)
+
+    return trans_mat, channel_mapper, channel_unmapper
 
 
-def fit_markov():
+def markov_memory(iterable, k_order):
+    """
+    Returns the ordered k_order tuples and number of occurrences of each
+    for the specified path.
+
+    Adapted from an itertools recipe.
+    """
+    a, b = itertools.tee(iterable)
+    ordered_pairs = list(zip(a, itertools.islice(b, 1, None)))
+    # d = dict(collections.Counter(ordered_pairs))
+    # e = collections.defaultdict(dict)
+    #
+    # for (c1, c2) in ordered_pairs:
+    #     e[c1] = collections.defaultdict(int)
+    #     e[c1][c2] += d[(c1, c2)]
+
+    # return d
+    return ordered_pairs
+
+
+def fit_markov(df, paths, conversions, sep, revenues=None, costs=None):
     """Markov attribution model."""
-    pass
+    trans_mat, channel_mapper, channel_unmapper = \
+        _get_markov_components(df, paths)
 
-
+    return trans_mat, channel_mapper, channel_unmapper
